fa7bf0f80c3ae36a60b003a445544d7105e4aed8
ruoi67 <98221447+ruoi67@users.noreply.github.com>
2022-01-27 20:13:53 +0300

    Delete yyy.cmd

--- yyy.cmd
+++
@@ -1,7 +0,0 @@
-echo "# thebestgames2022.github.com" >> README.md 
-git init 
-git add README.md 
-git commit -m "first commit" 
-git branch -M main 
-git remote add origin https://github.com/ruoi67/thebestgames2022. github.com.git
- git push -u origin main
